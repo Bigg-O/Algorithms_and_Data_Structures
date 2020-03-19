581919eb4f37a7114746ac0bd23c3f7cd36f789a
Wookeun Song <wookeun.s@gmail.com>
2020-03-19 14:42:47 -0500

    best answer

--- JavaScript/LeetCode/1.Two_Sum.js
+++ JavaScript/LeetCode/1.Two_Sum.js
@@ -26,36 +26,65 @@
 // };
 
 // One-pass Hash Table
-function twoSum(nums, target) {
+// function twoSum(nums, target) {
+//     const numTable = {}
+//     let i;
+
+//     for (i = 0; i < nums.length; i++) {
+//         if (numTable[nums[i]]) {
+//             numTable[nums[i]]++
+//         } else {
+//             numTable[nums[i]] = 1
+//         }
+
+//         // duplacate number case
+//         if (target - nums[i] === nums[i]) {
+//             if (numTable[nums[i]] > 1) {
+//                 break
+//             }
+//         } else {
+//             // none-duplcate number case
+//             if (numTable[target - nums[i]]) {
+//                 break
+//             }
+//         }
+
+//     }
+
+//     if (i < nums.length) {
+//         return [nums.indexOf(target - nums[i]), i]
+//     } else {
+//         return []
+//     }
+// };
+
+// var twoSum = function (nums, target) {
+//     const numTable = {};
+
+//     for (let i = 0; i < nums.length; i++) {
+//         let otherPair = target - nums[i];
+
+//         if (otherPair in numTable) {
+//             return [numTable[otherPair], i]
+//         }
+
+//         numTable[nums[i]] = i;
+//     }
+// }
+
+// One Pass Hash Table Best Solution
+var twoSum = function (nums, target) {
     const numTable = {}
-    let i;
 
-    for (i = 0; i < nums.length; i++) {
-        if (numTable[nums[i]]) {
-            numTable[nums[i]]++
-        } else {
-            numTable[nums[i]] = 1
+    for (let i = 0; i < nums.length; i++) {
+        let otherPair = target - nums[i];
+
+        if (otherPair in numTable) {
+            return [numTable[otherPair], i]
         }
 
-        // duplacate number case
-        if (target - nums[i] === nums[i]) {
-            if (numTable[nums[i]] > 1) {
-                break
-            }
-        } else {
-            // none-duplcate number case
-            if (numTable[target - nums[i]]) {
-                break
-            }
-        }
-
+        numTable[nums[i]] = i;
     }
-
-    if (i < nums.length) {
-        return [nums.indexOf(target - nums[i]), i]
-    } else {
-        return []
-    }
-};
+}
 
 console.log("RESULT:", twoSum([3, 2, 4], 6))
