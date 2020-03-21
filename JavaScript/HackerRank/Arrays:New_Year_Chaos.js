ff31abd9ed3711526c176fcad16fbbb03810a9ac
Wookeun Song <wookeun.s@gmail.com>
2020-03-21 17:12:13 -0500

    correct answer

--- JavaScript/HackerRank/Arrays:New_Year_Chaos.js
+++ JavaScript/HackerRank/Arrays:New_Year_Chaos.js
@@ -1,20 +1,30 @@
 function minimumBribes(line) {
-    let result = 0
+    let result = 0;
+    let min = line.length
 
     for (let i = line.length - 1; i >= 0; i--) {
         if (i + 3 < line[i]) {
-            return"Too chaotic"
+            return `Too chaotic`;
         }
 
-        if (i + 1 < line[i]) {
-            result += line[i] - (i + 1)
+        if (line[i] > i + 1) {
+            result += line[i] - (i + 1);
+        } else {
+            // Checking if upcoming number is smaller than min
+            if (min > line[i]) {
+                min = line[i];
+            } else if (line[i] !== min) {
+                result++;
+            }
         }
+        
+        console.log(min, result)
     }
 
-    return result
+    return result;
 }
 
-const line = [1, 2, 5, 3, 4, 7, 8, 6]
+const line = [1, 2, 5, 3, 4, 7, 8, 6, 4]
 
 console.log("INPUT:", line)
 console.log("RESULT:", minimumBribes(line))
