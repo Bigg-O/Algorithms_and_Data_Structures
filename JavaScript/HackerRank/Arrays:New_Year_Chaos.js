5586517e61630e1c11d0b01725045536b5c7d346
Wookeun Song <wookeun.s@gmail.com>
2020-03-21 16:45:20 -0500

    almost working

--- JavaScript/HackerRank/Arrays:New_Year_Chaos.js
+++ JavaScript/HackerRank/Arrays:New_Year_Chaos.js
@@ -1,18 +1,13 @@
 function minimumBribes(line) {
-    for (let i = 1; i <= line.length - 2; i++) {
-        if (i + 2 < line[i]) {
-            return "Too chaotic"
-        }
-    }
-
     let result = 0
-    for (let i = line.length; i > 0; i--) {
-        if (line[i] > i) {
-            let j = i
-            while (j === line[i]) {
-                j--
-                result++
-            }
+
+    for (let i = line.length - 1; i >= 0; i--) {
+        if (i + 3 < line[i]) {
+            return"Too chaotic"
+        }
+
+        if (i + 1 < line[i]) {
+            result += line[i] - (i + 1)
         }
     }
 
@@ -20,4 +15,6 @@ function minimumBribes(line) {
 }
 
 const line = [1, 2, 5, 3, 4, 7, 8, 6]
-console.log(minimumBribes(line))
+
+console.log("INPUT:", line)
+console.log("RESULT:", minimumBribes(line))
