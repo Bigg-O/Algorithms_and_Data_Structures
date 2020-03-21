4ebd3f60bbcbf54ecbc7f847977e6111cf2cf49d
Wookeun Song <wookeun.s@gmail.com>
2020-03-21 17:40:41 -0500

    selection sort method

--- JavaScript/HackerRank/Arrays/Minimum_Swaps_2.js
+++ JavaScript/HackerRank/Arrays/Minimum_Swaps_2.js
@@ -1,3 +1,30 @@
 function minimumSwaps(arr) {
+    let result = 0
+    let arrCopy = [...arr]
 
+    // Selection Sort
+    for (let i = 0; i < arrCopy.length; i++) {
+        let minIndex = null;
+        let min = arrCopy[i]
+
+        for (let j = i; j < arrCopy.length; j++) {
+            if (min > arrCopy[j]) {
+                min = arrCopy[j]
+                minIndex = j
+            }
+        }
+
+        if (minIndex) {
+            const temp = arrCopy[i]
+            arrCopy[i] = min
+            arrCopy[minIndex] = temp
+            result++
+        }
+    }
+
+    return result
 }
+
+const input = [2, 3, 4, 1, 5]
+console.log("INPUT:", input)
+console.log("OUTPUT:", minimumSwaps(input))
