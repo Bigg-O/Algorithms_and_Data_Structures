d505c0edf35b109e56ebca6243a3e30c64d815e1
Wookeun Song <wookeun.s@gmail.com>
2020-03-21 18:04:15 -0500

    best way

--- JavaScript/HackerRank/Arrays/Minimum_Swaps_2.js
+++ JavaScript/HackerRank/Arrays/Minimum_Swaps_2.js
@@ -1,29 +1,60 @@
+// Map Method
+// Big O = O(n)
 function minimumSwaps(arr) {
+    const obj = {}
     let result = 0
-    let arrCopy = [...arr]
 
-    // Selection Sort
-    for (let i = 0; i < arrCopy.length; i++) {
-        let minIndex = null;
-        let min = arrCopy[i]
+    for (let i = 0; i < arr.length; i++) {
+        obj[arr[i]] = i
+    }
 
-        for (let j = i; j < arrCopy.length; j++) {
-            if (min > arrCopy[j]) {
-                min = arrCopy[j]
-                minIndex = j
-            }
-        }
+    for (let i = 0; i < arr.length; i++) {
+        const expectedNum = i + 1
+        const actualNum = arr[i]
 
-        if (minIndex) {
-            const temp = arrCopy[i]
-            arrCopy[i] = min
-            arrCopy[minIndex] = temp
-            result++
+        if (actualNum !== expectedNum) {
+            let whereToMove = obj[expectedNum];
+
+            arr[i] = expectedNum;
+            obj[expectedNum] = i;
+
+            arr[whereToMove] = actualNum;
+            obj[actualNum] = whereToMove;
+
+            result++;
         }
     }
 
-    return result
+    return result;
 }
+
+// Selection Sort Method
+// Big O = O(n^2)
+// function minimumSwaps(arr) {
+//     let result = 0
+//     let arrCopy = [...arr]
+
+//     for (let i = 0; i < arrCopy.length; i++) {
+//         let minIndex = null;
+//         let min = arrCopy[i]
+
+//         for (let j = i; j < arrCopy.length; j++) {
+//             if (min > arrCopy[j]) {
+//                 min = arrCopy[j]
+//                 minIndex = j
+//             }
+//         }
+
+//         if (minIndex) {
+//             const temp = arrCopy[i]
+//             arrCopy[i] = min
+//             arrCopy[minIndex] = temp
+//             result++
+//         }
+//     }
+
+//     return result
+// }
 
 const input = [2, 3, 4, 1, 5]
 console.log("INPUT:", input)
