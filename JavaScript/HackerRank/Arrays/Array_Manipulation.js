b8222e8f9630926c6dbe7025ef05a890b8cc540b
Wookeun Song <wookeun.s@gmail.com>
2020-03-25 02:42:33 -0500

    comments

--- JavaScript/HackerRank/Arrays/Array_Manipulation.js
+++ JavaScript/HackerRank/Arrays/Array_Manipulation.js
@@ -1,14 +1,39 @@
-function arrayManipulation(n, queries) {
-    const board = []
-    for (let i = 0; i < n; i++) board[i] = 0
+// // My Solution
+// function arrayManipulation(n, queries) {
+//     const board = []
+//     for (let i = 0; i < n; i++) board[i] = 0
 
-    for (let i = 0; i < queries[0].length; i++) {
-        for (let j = queries[0][i] - 1; j < queries[1][i]; j++) {
-            board[j] += queries[2][i]
-        }
+//     for (let i = 0; i < queries[0].length; i++) {
+//         for (let j = queries[0][i] - 1; j < queries[1][i]; j++) {
+//             board[j] += queries[2][i]
+//         }
+//     }
+
+//     return Math.max(...board)
+// }
+
+// Best Solution
+function arrayManipulation(n, queries) {
+    const arr = []
+    for (let i = 0; i < n; i++) {
+        arr[i] = 0;
     }
 
-    return Math.max(...board)
+    // for each sum operation in queries
+    // update arr with number to add at index = queries[i][0]
+    // and number to remove at index = queries[i][0]+1
+    // this will allow us to build each element of the final array by summing all elements
+    for (let i = 0; i < queries.length; i++) {
+        arr[queries[i][0] - 1] += queries[i][2];
+        if (queries[i][1] < arr.length) {
+            arr[queries[i][1]] -= queries[i][2];
+        }
+    }
+    for (let i = 1; i < n; i++) {
+        arr[i] += arr[i - 1];
+    }
+
+    return Math.max(...arr)
 }
 
 // const n = 10
