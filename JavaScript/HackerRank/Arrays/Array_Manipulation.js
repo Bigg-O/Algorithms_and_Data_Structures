1cafa0f56eb8c83e8ff624e18af2353d915de99d
Wookeun Song <wookeun.s@gmail.com>
2020-03-25 02:25:12 -0500

    best answer?

--- JavaScript/HackerRank/Arrays/Array_Manipulation.js
+++ JavaScript/HackerRank/Arrays/Array_Manipulation.js
@@ -1,25 +1,22 @@
 function arrayManipulation(n, queries) {
     const board = []
-    const qLen = queries[0].length
+    for (let i = 0; i < n; i++) board[i] = 0
 
-    for (let i = 0; i < qLen; i++) {
-        for (let j = 0; j < n; j++) {
-            if (board[j] === undefined) {
-                board[j] = 0
-            }
-            if (j + 1 >= queries[0][i] && j + 1 <= queries[1][i]) {
-                board[j] += queries[2][i]
-            }
+    for (let i = 0; i < queries[0].length; i++) {
+        for (let j = queries[0][i] - 1; j < queries[1][i]; j++) {
+            board[j] += queries[2][i]
         }
     }
 
     return Math.max(...board)
 }
 
-const n = 10
-const queries = [[2, 3, 1, 5], [6, 5, 8, 9], [8, 7, 1, 15]]
-// const n = 5
-// const queries = [[1, 2, 3], [2, 5, 4], [100, 100, 100]]
+// const n = 10
+// const queries = [[2, 3, 1, 5], [6, 5, 8, 9], [8, 7, 1, 15]]
+const n = 5
+const queries = [[1, 2, 3], [2, 5, 4], [100, 100, 100]]
+// const n = 10
+// const queries = [[1, 5, 6], [3, 8, 9], [3, 7, 1]]
 
 console.log("INPUT: n =", n)
 console.log("INPUT: queries =", queries)
