363bba993a3d77aa7183896809a78333b012023f
Wookeun Song <wookeun.s@gmail.com>
2020-03-25 18:27:05 -0500

    brute force

--- JavaScript/HackerRank/Arrays/Array_Manipulation.js
+++ JavaScript/HackerRank/Arrays/Array_Manipulation.js
@@ -1,38 +1,14 @@
-// // My Solution
-// function arrayManipulation(n, queries) {
-//     const board = []
-//     for (let i = 0; i < n; i++) board[i] = 0
-
-//     for (let i = 0; i < queries[0].length; i++) {
-//         for (let j = queries[0][i] - 1; j < queries[1][i]; j++) {
-//             board[j] += queries[2][i]
-//         }
-//     }
-
-//     return Math.max(...board)
-// }
-
-// Best Solution
+// My first real solution
 function arrayManipulation(n, queries) {
     const arr = []
     for (let i = 0; i < n; i++) {
-        arr[i] = 0;
+        arr[i] = 0
     }
 
-    // for each sum operation in queries
-    // update arr with number to add at index = queries[i][0]
-    // and number to remove at index = queries[i][0]+1
-    // this will allow us to build each element of the final array by summing all elements
     for (let i = 0; i < queries.length; i++) {
-        console.log(arr)
-        arr[queries[i][0] - 1] += queries[i][2];
-        if (queries[i][1] < arr.length) {
-            arr[queries[i][1]] -= queries[i][2];
+        for (let j = queries[i][0] - 1; j < queries[i][1]; j++) {
+            arr[j] += queries[i][2]
         }
-    }
-    for (let i = 1; i < n; i++) {
-        console.log(arr)
-        arr[i] += arr[i - 1];
     }
 
     let result = 0
@@ -45,13 +21,27 @@ function arrayManipulation(n, queries) {
     return result
 }
 
-// const n = 10
-// const queries = [[2, 6, 8], [3, 5, 7], [1, 8, 1], [5, 9, 15]]
-const n = 5
-const queries = [[1, 2, 100], [2, 5, 100], [3, 4, 100]]
+const n = 10
+const queries = [[2, 6, 8], [3, 5, 7], [1, 8, 1], [5, 9, 15]]
+// const n = 5
+// const queries = [[1, 2, 100], [2, 5, 100], [3, 4, 100]]
 // const n = 10
 // const queries = [[1, 5, 3], [4, 8, 7], [6, 9, 1]]
 
 console.log("INPUT: n =", n)
 console.log("INPUT: queries =", queries)
 console.log("OUTPUT: ", arrayManipulation(n, queries))
+
+// // My Solution for wrong input
+// function arrayManipulation(n, queries) {
+//     const board = []
+//     for (let i = 0; i < n; i++) board[i] = 0
+
+//     for (let i = 0; i < queries[0].length; i++) {
+//         for (let j = queries[0][i] - 1; j < queries[1][i]; j++) {
+//             board[j] += queries[2][i]
+//         }
+//     }
+
+//     return Math.max(...board)
+// }
