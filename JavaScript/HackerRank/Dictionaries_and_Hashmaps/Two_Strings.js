669b98b49b81f7da14800a63fd2e8ce18552f7fd
Wookeun Song <wookeun.s@gmail.com>
2020-03-25 23:13:20 -0500

    second solution

--- JavaScript/HackerRank/Dictionaries_and_Hashmaps/Two_Strings.js
+++ JavaScript/HackerRank/Dictionaries_and_Hashmaps/Two_Strings.js
@@ -1,6 +1,10 @@
 // Second Solution
 function twoStrings(s1, s2) {
-    
+    if (s1.split('').filter(char => s2.includes(char)).length) {
+        return "YES"
+    } else {
+        return "NO"
+    }
 }
 
 // const s1 = "hello"
@@ -12,25 +16,25 @@ console.log("s1", s1)
 console.log("s2:", s2)
 console.log("OUTPUT:", twoStrings(s1, s2))
 
-// First Solution
-function twoStrings(s1, s2) {
-    const letters = {}
-    let bgStr = s1;
-    let smStr = s2;
-    if (s1.length > s2.length) {
-        bgStr = s2
-        smStr = s1
-    }
+// // First Solution
+// function twoStrings(s1, s2) {
+//     const letters = {}
+//     let bgStr = s1;
+//     let smStr = s2;
+//     if (s1.length > s2.length) {
+//         bgStr = s2
+//         smStr = s1
+//     }
 
-    for (const char of bgStr) {
-        if (!letters[char]) {
-            letters[char] = true
-        }
-    }
+//     for (const char of bgStr) {
+//         if (!letters[char]) {
+//             letters[char] = true
+//         }
+//     }
 
-    for (const char of smStr) {
-        if (letters[char]) return "YES"
-    }
+//     for (const char of smStr) {
+//         if (letters[char]) return "YES"
+//     }
 
-    return "NO"
-}
+//     return "NO"
+// }
