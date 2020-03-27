c878b95e6a2a962bdadf3f0891a9a524252009bc
Wookeun Song <wookeun.s@gmail.com>
2020-03-26 22:16:54 -0500

    failed

--- JavaScript/HackerRank/Dictionaries_and_Hashmaps/Sherlock_and_Anagrams.js
+++ JavaScript/HackerRank/Dictionaries_and_Hashmaps/Sherlock_and_Anagrams.js
@@ -1,3 +1,34 @@
-function sherlockAndAnagrams(s) {
+// // My Solution
+// function sherlockAndAnagrams(str) {
+//     let result = 0
 
+
+
+//     return result
+// }
+
+const str = "kkkk"
+
+console.log("str:", str)
+console.log("OUTPUT:", sherlockAndAnagrams(str))
+
+// Best HackerRanck Solution
+function sherlockAndAnagrams(s) {
+    let result = 0;
+    const charMap = {};
+
+    for (let i = 1; i < s.length; i++) {
+        for (let j = 0; j < s.length - i + 1; j++) {
+            const subStr = s.slice(j, j + i).split('').sort().join('');
+            charMap[subStr] = (charMap[subStr] || 0) + 1
+        }
+    }
+
+    for (const num of Object.values(charMap)) {
+        if (num > 1)
+            console.log(num)
+        result += num * (num - 1) / 2;
+    }
+    
+    return result
 }
