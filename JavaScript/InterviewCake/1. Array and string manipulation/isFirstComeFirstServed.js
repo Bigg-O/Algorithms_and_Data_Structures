37290ebfe8b469696a6bd7a2d3719e103be9b995
Wookeun Song <wookeun.s@gmail.com>
2020-03-28 03:00:51 -0500

    done

--- JavaScript/InterviewCake/1. Array and string manipulation/isFirstComeFirstServed.js
+++ JavaScript/InterviewCake/1. Array and string manipulation/isFirstComeFirstServed.js
@@ -1,20 +1,40 @@
+// Recursive
 function isFirstComeFirstServed(takeOutOrders, dineInOrders, servedOrders) {
-    if (takeOutOrders.length + dineInOrders.length > servedOrders.length) {
+    if (takeOutOrders.length + dineInOrders.length !== servedOrders.length) {
         return false
     }
-
-    let takeOutIndex = 0
-    let dineInIndex = 0
-
-    for (const order of servedOrders) {
-        if (takeOutIndex < takeOutOrders.length && order === takeOutOrders[takeOutIndex]) {
-            takeOutIndex++
-        } else if (dineInIndex < dineInOrders.length && order === dineInOrders[dineInIndex]) {
-            dineInIndex++
-        } else {
-            return false
-        }
+    if (!servedOrders.length) {
+        return true
     }
 
-    return true
+    const currentOrder = servedOrders.shift()
+    if (takeOutOrders[0] === currentOrder) {
+        takeOutOrders.shift()  
+    } else if (dineInOrders[0] === currentOrder) {
+        dineInOrders.shift()
+    }
+
+    return isFirstComeFirstServed(takeOutOrders, dineInOrders, servedOrders)
 }
+
+// // Iterative
+// function isFirstComeFirstServed(takeOutOrders, dineInOrders, servedOrders) {
+//     if (takeOutOrders.length + dineInOrders.length !== servedOrders.length) {
+//         return false
+//     }
+
+//     let takeOutIndex = 0
+//     let dineInIndex = 0
+
+//     for (const order of servedOrders) {
+//         if (takeOutIndex < takeOutOrders.length && order === takeOutOrders[takeOutIndex]) {
+//             takeOutIndex++
+//         } else if (dineInIndex < dineInOrders.length && order === dineInOrders[dineInIndex]) {
+//             dineInIndex++
+//         } else {
+//             return false
+//         }
+//     }
+
+//     return true
+// }
