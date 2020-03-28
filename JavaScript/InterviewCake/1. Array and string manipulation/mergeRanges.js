0bef7247a0cdc01d46dd2f0de1aeb6fe5efe156d
Wookeun Song <wookeun.s@gmail.com>
2020-03-28 00:25:45 -0500

    first solution

--- JavaScript/InterviewCake/1. Array and string manipulation/mergeRanges.js
+++ JavaScript/InterviewCake/1. Array and string manipulation/mergeRanges.js
@@ -1,110 +1,28 @@
 function mergeRanges(meetings) {
+  meetings = meetings.sort((a, b) => a.startTime - b.startTime)
 
-    // console.log(meetings)
-    
-  
-    return [];
-  }
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  // Tests
-  
-  let desc = 'meetings overlap';
-  let actual = mergeRanges([{ startTime: 1, endTime: 3 }, { startTime: 2, endTime: 4 }]);
-  let expected = [{ startTime: 1, endTime: 4 }];
-  assertArrayEquals(actual, expected, desc);
-  
-  desc = 'meetings touch';
-  actual = mergeRanges([{ startTime: 5, endTime: 6 }, { startTime: 6, endTime: 8 }]);
-  expected = [{ startTime: 5, endTime: 8 }];
-  assertArrayEquals(actual, expected, desc);
-  
-  desc = 'meeting contains other meeting';
-  actual = mergeRanges([{ startTime: 1, endTime: 8 }, { startTime: 2, endTime: 5 }]);
-  expected = [{ startTime: 1, endTime: 8 }];
-  assertArrayEquals(actual, expected, desc);
-  
-  desc = 'meetings stay separate';
-  actual = mergeRanges([{ startTime: 1, endTime: 3 }, { startTime: 4, endTime: 8 }]);
-  expected = [{ startTime: 1, endTime: 3 }, { startTime: 4, endTime: 8 }];
-  assertArrayEquals(actual, expected, desc);
-  
-  desc = 'multiple merged meetings';
-  actual = mergeRanges([
-    { startTime: 1, endTime: 4 },
-    { startTime: 2, endTime: 5 },
-    { startTime: 5, endTime: 8 },
-  ]);
-  expected = [{ startTime: 1, endTime: 8 }];
-  assertArrayEquals(actual, expected, desc);
-  
-  desc = 'meetings not sorted';
-  actual = mergeRanges([
-    { startTime: 5, endTime: 8 },
-    { startTime: 1, endTime: 4 },
-    { startTime: 6, endTime: 8 },
-  ]);
-  expected = [{ startTime: 1, endTime: 4 }, { startTime: 5, endTime: 8 }];
-  assertArrayEquals(actual, expected, desc);
-  
-  desc = 'oneLongMeetingContainsSmallerMeetings';
-  actual = mergeRanges([
-    { startTime: 1, endTime: 10 },
-    { startTime: 2, endTime: 5 },
-    { startTime: 6, endTime: 8 },
-    { startTime: 9, endTime: 10 },
-    { startTime: 10, endTime: 12 }
-  ]);
-  expected = [
-    { startTime: 1, endTime: 12 }
-  ];
-  assertArrayEquals(actual, expected, desc);
-  
-  desc = 'sample input';
-  actual = mergeRanges([
-    { startTime: 0, endTime: 1 },
-    { startTime: 3, endTime: 5 },
-    { startTime: 4, endTime: 8 },
-    { startTime: 10, endTime: 12 },
-    { startTime: 9, endTime: 10 },
-  ]);
-  expected = [
-    { startTime: 0, endTime: 1 },
-    { startTime: 3, endTime: 8 },
-    { startTime: 9, endTime: 12 },
-  ];
-  assertArrayEquals(actual, expected, desc);
-  
-  function assertArrayEquals(a, b, desc) {
-    // Sort the keys in each meeting to avoid
-    // failing based on differences in key order.
-    orderedA = a.map( function(meeting) {
-      return JSON.stringify(meeting, Object.keys(meeting).sort());
-    });
-    orderedB = b.map( function(meeting) {
-      return JSON.stringify(meeting, Object.keys(meeting).sort());
-    });
-    const arrayA = JSON.stringify(orderedA);
-    const arrayB = JSON.stringify(orderedB);
-    if (arrayA !== arrayB) {
-      console.log(`${desc} ... FAIL: ${JSON.stringify(a)} != ${JSON.stringify(b)}`)
+  let i = 0
+  while (i < meetings.length - 1) {
+    // current End >= next Start
+    if (meetings[i].endTime >= meetings[i + 1].startTime) {
+      // current End < next End
+      if (meetings[i].endTime < meetings[i + 1].endTime) {
+        meetings[i].endTime = meetings[i + 1].endTime
+      }
+      meetings.splice(i + 1, 1)
     } else {
-      console.log(`${desc} ... PASS`);
+      i++
     }
   }
+
+  return meetings;
+}
+
+const meetings = [{ startTime: 0, endTime: 1 },
+{ startTime: 3, endTime: 5 },
+{ startTime: 4, endTime: 8 },
+{ startTime: 10, endTime: 12 },
+{ startTime: 9, endTime: 10 }]
+
+console.log("meetings:", meetings)
+console.log("OUTPUT:", mergeRanges(meetings))
