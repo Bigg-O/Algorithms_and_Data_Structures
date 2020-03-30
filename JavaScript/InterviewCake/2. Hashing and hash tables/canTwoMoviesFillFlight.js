3cbbadfe04720b4b9d0f1df3e3aafc07061788ba
Wookeun Song <wookeun.s@gmail.com>
2020-03-29 20:18:14 -0500

    set

--- JavaScript/InterviewCake/2. Hashing and hash tables/canTwoMoviesFillFlight.js
+++ JavaScript/InterviewCake/2. Hashing and hash tables/canTwoMoviesFillFlight.js
@@ -1,24 +1,34 @@
-// Hash Map solution
-function canTwoMoviesFillFlight(movieLengths, flightLength) {
-    const lengthMap = {}
-    for (const length of movieLengths) {
-        lengthMap[length] = (lengthMap[length] || 0) + 1
-    }
-
-    for (const length in lengthMap) {
-        if (target == length) {
-            if (lengthMap[target] > 1) {
-                return true
-            }
-        } else if (lengthMap[target]) {
-            return true
-        }
-    }
-
-    return false;
-}
-
-// // Set Solution
+// // Hash Map solution
 // function canTwoMoviesFillFlight(movieLengths, flightLength) {
+//     const lengthMap = {}
+//     for (const length of movieLengths) {
+//         lengthMap[length] = (lengthMap[length] || 0) + 1
+//     }
 
+//     for (const length in lengthMap) {
+//         const target = flightLength - length
+//         if (target == length) {
+//             if (lengthMap[target] > 1) {
+//                 return true
+//             }
+//         } else if (lengthMap[target]) {
+//             return true
+//         }
+//     }
+
+//     return false;
 // }
+
+// Set Solution
+function canTwoMoviesFillFlight(movieLengths, flightLength) {
+    const lengthSet = new Set()
+
+    for (const length of movieLengths) {
+        const target = flightLength - length
+
+        if (lengthSet.has(target)) return true
+        else lengthSet.add(length)
+    }
+
+    return false
+}
