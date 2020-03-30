9f09d738c1f2cfdb3acef1bc37ab9542929e7d80
Wookeun Song <wookeun.s@gmail.com>
2020-03-30 16:57:41 -0500

    better sol

--- JavaScript/LeetCode/1342.Number_of_Steps_to_Reduce_a_Number_to_Zero.js
+++ JavaScript/LeetCode/1342.Number_of_Steps_to_Reduce_a_Number_to_Zero.js
@@ -1,23 +1,21 @@
 // // Iterative
 // var numberOfSteps  = function(num) {
 //    let result = 0 
-    
+
 //     while (num) {
 //         if (num % 2) num--
 //         else num /= 2
 //         result++
 //     }
-    
+
 //     return result
 // }
 
 // Recursive
-var numberOfSteps  = function(num, result = -1) {
-    result++
-    
+var numberOfSteps = function (num) {
     if (!num) {
         return result
     }
-    
-    return numberOfSteps(num % 2 ? num -1 : num / 2, result)
+
+    return 1 + numberOfSteps(num % 2 ? num - 1 : num / 2)
 }
