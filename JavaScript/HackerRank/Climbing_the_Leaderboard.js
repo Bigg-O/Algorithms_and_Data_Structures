fd542ea368d43996695a21358185c83930d9eed5
Wookeun Song <wookeun.s@gmail.com>
2020-04-01 00:59:17 -0500

    second solution

--- JavaScript/HackerRank/Climbing_the_Leaderboard.js
+++ JavaScript/HackerRank/Climbing_the_Leaderboard.js
@@ -1,7 +1,6 @@
-// First Bute Force Solution O(n^2)
+// Second Solution O(n * log(m)) ?
 function climbingLeaderboard(scores, alice) {
-    scores = [...new Set(scores)].sort((a, b) => b - a)
-    alice = alice.sort((a, b) => a - b)
+    scores = [...new Set(scores)]
     const result = []
 
     for (let i = 0; i < alice.length; i++) {
@@ -10,7 +9,10 @@ function climbingLeaderboard(scores, alice) {
             continue;
         }
 
-        for (let j = scores.length - 1; j >= 0; j--) {
+        let j = scores.length - 1
+        if (result.length) j = result[result.length - 1] - 1
+
+        for (j; j >= 0; j--) {
             if (scores[j] === alice[i]) {
                 result.push(j + 1)
                 break
@@ -23,3 +25,28 @@ function climbingLeaderboard(scores, alice) {
 
     return result
 }
+
+// // First Bute Force Solution O(n * m)
+// function climbingLeaderboard(scores, alice) {
+//     scores = [...new Set(scores)]
+//     const result = []
+
+//     for (let i = 0; i < alice.length; i++) {
+//         if (scores[0] < alice[i]) {
+//             result.push(1)
+//             continue;
+//         }
+
+//         for (let j = scores.length - 1; j >= 0; j--) {
+//             if (scores[j] === alice[i]) {
+//                 result.push(j + 1)
+//                 break
+//             } else if (scores[j] > alice[i]) {
+//                 result.push(j + 2)
+//                 break
+//             }
+//         }
+//     }
+
+//     return result
+// }
