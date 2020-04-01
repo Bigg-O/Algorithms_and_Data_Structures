81922a7ce998566276370128f1d6d22be122752a
Wookeun Song <wookeun.s@gmail.com>
2020-04-01 00:37:26 -0500

    first sol

--- JavaScript/HackerRank/Climbing_the_Leaderboard.js
+++ JavaScript/HackerRank/Climbing_the_Leaderboard.js
@@ -1,4 +1,25 @@
+// First Bute Force Solution O(n^2)
 function climbingLeaderboard(scores, alice) {
+    scores = [...new Set(scores)].sort((a, b) => b - a)
+    alice = alice.sort((a, b) => a - b)
+    const result = []
 
+    for (let i = 0; i < alice.length; i++) {
+        if (scores[0] < alice[i]) {
+            result.push(1)
+            continue;
+        }
 
+        for (let j = scores.length - 1; j >= 0; j--) {
+            if (scores[j] === alice[i]) {
+                result.push(j + 1)
+                break
+            } else if (scores[j] > alice[i]) {
+                result.push(j + 2)
+                break
+            }
+        }
+    }
+
+    return result
 }
