45cd2bb94f1815448a9f58463b68a5e7d2834476
Wookeun Song <wookeun.s@gmail.com>
2020-04-01 01:05:40 -0500

    Shape Finder

---
+++ JavaScript/Shape_Finder.js
@@ -0,0 +1,19 @@
+/* 
+Create a function that can take a multi dimensional array of Pixels and output how many shapes are present. Pixels will be either black or white. 
+A shape is defined as a contiguous pattern of one or more black pixels in 4 dimensions: up, down, left, right, and not diagonal.
+Example: matrix below has 2 shapes,
+*/
+
+
+function shapes(points) {
+  
+}
+
+const points = [
+  ["b", "w", "b", "b", "b"],
+  ["b", "w", "b", "w", "w"],
+  ["b", "w", "b", "b", "b"]
+]
+
+console.log("points:", points)
+console.log("OUTPUT:", shapes(points))
