0b92f1f167897f44d25a5a644aba327b642bd043
Wookeun Song <wookeun.s@gmail.com>
2020-04-02 19:56:40 -0500

    FS

--- JavaScript/InterviewCake/2.Hashing_and_hash_tables/Permutation_Palindrome.js
+++ JavaScript/InterviewCake/2.Hashing_and_hash_tables/Permutation_Palindrome.js
@@ -1,7 +1,16 @@
+// First Solution O(n)
 function hasPalindromePermutation(theString) {
+    const charMap = {}
 
-    // Check if any permutation of the input is a palindrome
+    for (const char of theString) {
+      charMap[char] = (charMap[char] || 0) + 1
+    }
     
-  
-    return false;
-  }
+    let oddCount = 0
+    for (const num of Object.values(charMap)) {
+      if (num % 2 === 1) oddCount++
+      if (oddCount > 1) return false
+    }
+    
+    return true
+}
