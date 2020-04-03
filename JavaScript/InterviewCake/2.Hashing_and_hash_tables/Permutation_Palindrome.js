f862671b61f1ee81f3be601778df71eebd12909e
Wookeun Song <wookeun.s@gmail.com>
2020-04-02 20:01:17 -0500

    Set Solution

--- JavaScript/InterviewCake/2.Hashing_and_hash_tables/Permutation_Palindrome.js
+++ JavaScript/InterviewCake/2.Hashing_and_hash_tables/Permutation_Palindrome.js
@@ -1,16 +1,27 @@
+// Set Solution O(n)
+function hasPalindromePermutation(theString) {
+    const charSet = new Set()
+
+    for (const char of theString) {
+        charSet.has(char) ? charSet.delete(char) : charSet.add(char)
+    }
+
+    return charSet.size < 2
+}
+
 // First Solution O(n)
 function hasPalindromePermutation(theString) {
     const charMap = {}
 
     for (const char of theString) {
-      charMap[char] = (charMap[char] || 0) + 1
+        charMap[char] = (charMap[char] || 0) + 1
     }
-    
+
     let oddCount = 0
     for (const num of Object.values(charMap)) {
-      if (num % 2 === 1) oddCount++
-      if (oddCount > 1) return false
+        if (num % 2 === 1) oddCount++
+        if (oddCount > 1) return false
     }
-    
+
     return true
 }
