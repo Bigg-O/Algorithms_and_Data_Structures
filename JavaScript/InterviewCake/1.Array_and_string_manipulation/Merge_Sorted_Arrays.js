27b2487dbb682de978f76de56999b847dec153b0
Wookeun Song <wookeun.s@gmail.com>
2020-04-05 00:32:20 -0500

    First Solution

--- JavaScript/InterviewCake/1.Array_and_string_manipulation/Merge_Sorted_Arrays.js
+++ JavaScript/InterviewCake/1.Array_and_string_manipulation/Merge_Sorted_Arrays.js
@@ -1,3 +1,18 @@
+// First Destructive Solution
 function mergeArrays(myArray, alicesArray) {
-
-}
+    const result = []
+  
+    while (myArray.length && alicesArray.length) {
+      if (myArray[0] > alicesArray[0]) {
+        result.push(alicesArray.shift())
+      } else {
+        result.push(myArray.shift())
+      }
+    }
+    
+    if (myArray.length) {
+      return result.concat(myArray)
+    } else {
+      return result.concat(alicesArray)
+    }
+  }
