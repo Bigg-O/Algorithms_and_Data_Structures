c61424a5f6d90e18f15564ece6e8b9eee09336e0
Wookeun Song <wookeun.s@gmail.com>
2020-04-05 00:17:42 -0500

    update

--- JavaScript/InterviewCake/2.Hashing_and_hash_tables/Word_Cloud_Data.js
+++ JavaScript/InterviewCake/2.Hashing_and_hash_tables/Word_Cloud_Data.js
@@ -1,14 +1,14 @@
 class WordCloudData {
-    constructor(inputString) {
-      this.wordsToCounts = new Map();
-      this.populateWordsToCounts(inputString);
-    }
-  
-    populateWordsToCounts(inputString) {
-  
-      // Count the frequency of each word
-      
-  
-    }
-  
+  constructor(inputString) {
+    this.wordsToCounts = new Map();
+    this.populateWordsToCounts(inputString);
+  }
+
+  populateWordsToCounts(inputString) {
+
+    // Count the frequency of each word
+
+
+  }
+
 }
