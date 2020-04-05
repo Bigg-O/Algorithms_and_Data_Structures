755fdeee0079e78b903362e66e8aa92d4b8074ad
Wookeun Song <wookeun.s@gmail.com>
2020-04-05 00:51:53 -0500

    second solution

--- JavaScript/InterviewCake/1.Array_and_string_manipulation/Merge_Sorted_Arrays.js
+++ JavaScript/InterviewCake/1.Array_and_string_manipulation/Merge_Sorted_Arrays.js
@@ -1,18 +1,45 @@
-// First Destructive Solution
+// Second Solution Indestructive Solution
 function mergeArrays(myArray, alicesArray) {
     const result = []
   
-    while (myArray.length && alicesArray.length) {
-      if (myArray[0] > alicesArray[0]) {
-        result.push(alicesArray.shift())
+    let i = 0 // myArray Counter
+    let j = 0 // alicesArray Counter
+    
+    while (i < myArray.length && j < alicesArray.length) {
+      if (myArray[i] > alicesArray[j]) {
+        result.push(alicesArray[j])
+        j++
       } else {
-        result.push(myArray.shift())
+        result.push(myArray[i])
+        i++
       }
     }
     
-    if (myArray.length) {
-      return result.concat(myArray)
-    } else {
-      return result.concat(alicesArray)
+    for (i; i < myArray.length; i++) {
+      result.push(myArray[i])
     }
-  }
+    for (j; j < alicesArray.length; j++) {
+      result.push(alicesArray[j])
+    }
+    
+    return result
+}
+
+// // First Solution Destructive Solution
+// function mergeArrays(myArray, alicesArray) {
+//     const result = []
+  
+//     while (myArray.length && alicesArray.length) {
+//       if (myArray[0] > alicesArray[0]) {
+//         result.push(alicesArray.shift())
+//       } else {
+//         result.push(myArray.shift())
+//       }
+//     }
+    
+//     if (myArray.length) {
+//       return result.concat(myArray)
+//     } else {
+//       return result.concat(alicesArray)
+//     }
+// }
