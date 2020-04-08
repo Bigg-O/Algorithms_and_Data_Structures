03c7aae62db970fb6abf0031fb2c1c899a0de3d6
Wookeun Song <wookeun.s@gmail.com>
2020-04-07 23:22:49 -0500

    Was hard to figure out

--- JavaScript/InterviewCake/3.Greedy_algorithms/Apple_Stocks.js
+++ JavaScript/InterviewCake/3.Greedy_algorithms/Apple_Stocks.js
@@ -1,12 +1,37 @@
-// Second Solution
+// InterviewCake Solution 
+// Runtime O(n)
+// Space O(1)
 function getMaxProfit(stockPrices) {
-
+    if (stockPrices.length < 2) {
+      throw 'Getting a profit requires at least 2 prices'
+    }
+    
+    let min = stockPrices[0]
+    let result = stockPrices[1] - stockPrices[0]
+    
+    for (let i = 1; i < stockPrices.length; i++) {
+      if (stockPrices[i] - min > result) {
+        result = stockPrices[i] - min
+      }
+      if (stockPrices[i] < min) {
+          min = stockPrices[i]
+      }
+    }
+  
+    return result
 }
+  
 
 // // First Brute Force Solution
+// // Runtime
 // // O(n) at Best
 // // O(n^2) at Worst
+// // Space O(1)
 // function getMaxProfit(stockPrices) {
+//     if (stockPrices.length < 2) {
+//         throw 'Getting a profit requires at least 2 prices'
+//     }
+    
 //     let max = stockPrices[0]
 //     let min = stockPrices[0]
     
