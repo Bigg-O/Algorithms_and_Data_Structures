9663c790e1a5593f7fc2b95d59de9b1de525b9e3
Wookeun Song <wookeun.s@gmail.com>
2020-04-08 14:59:57 -0500

    first try

--- JavaScript/InterviewCake/3.Greedy_algorithms/Highest_Product_of_3.js
+++ JavaScript/InterviewCake/3.Greedy_algorithms/Highest_Product_of_3.js
@@ -1,7 +1,26 @@
+// First Try O(n)
+// Doesn't work with negative numbers
 function highestProductOf3(arrayOfInts) {
+    if (arrayOfInts.length < 4) {
+        throw 'error'
+    }
 
-    // Calculate the highest product of three numbers
-    
-  
-    return 0;
+    const biggestNums = []
+
+    for (let i = 0; i < 3; i++) {
+        let max = arrayOfInts[0]
+        let maxIndex = 0
+        
+        for (let i = 1; i < arrayOfInts.length; i++) {
+            if (arrayOfInts[i] > max) {
+                max = arrayOfInts[i]
+                maxIndex = i
+            }
+        }
+        
+        biggestNums.push(max)
+        arrayOfInts.splice(maxIndex, 1)
+    }
+
+    return biggestNums.reduce((a, b) => a * b)
 }
