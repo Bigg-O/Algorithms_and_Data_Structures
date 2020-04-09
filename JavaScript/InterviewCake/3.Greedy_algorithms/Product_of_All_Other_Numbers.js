c1ff6d72bbbd9af51a63e1d0df37be23bd3bc249
Wookeun Song <wookeun.s@gmail.com>
2020-04-08 19:29:06 -0500

    First Solution

--- JavaScript/InterviewCake/3.Greedy_algorithms/Product_of_All_Other_Numbers.js
+++ JavaScript/InterviewCake/3.Greedy_algorithms/Product_of_All_Other_Numbers.js
@@ -1,7 +1,23 @@
+// First Brite Force Solution
+// Runtime O(n^2)
+// Space O(n)
 function getProductsOfAllIntsExceptAtIndex(intArray) {
+    if (intArray.length < 2) {
+        throw 'error'
+    }
 
-    // Make a list of the products
-    
-  
-    return [];
+    let result = []
+
+    for (let i = 0; i < intArray.length; i++) {
+        let product = 1
+
+        for (let j = 0; j < intArray.length; j++) {
+            if (i === j) continue
+            product *= intArray[j]
+        }
+
+        result.push(product)
+    }
+
+    return result
 }
