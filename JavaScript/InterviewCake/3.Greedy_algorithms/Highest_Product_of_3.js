f6891e2f563f082c8e94d2e45c3b56beaa67f430
Wookeun Song <wookeun.s@gmail.com>
2020-04-08 19:06:42 -0500

    InterviewCake Solution

--- JavaScript/InterviewCake/3.Greedy_algorithms/Highest_Product_of_3.js
+++ JavaScript/InterviewCake/3.Greedy_algorithms/Highest_Product_of_3.js
@@ -1,38 +1,69 @@
-// Second Try
-// Runtime O(n)
-// Space O(1)
+// InterviewCake Solution 
 function highestProductOf3(arrayOfInts) {
-    if (arrayOfInts.length < 4) {
-        throw 'error'
+    if (arrayOfInts.length < 3) {
+      throw 'Less than 3 items!'
     }
   
-    let maxNums = [arrayOfInts.pop(), arrayOfInts.pop(), arrayOfInts.pop()]
-    
-    for (let count = 0; count < 3; count++) {
-      for (let i = 0; i < arrayOfInts.length; i++) {
-        const currentBiggest = maxNums.reduce((a,b) => a*b)
-        const originalNum = maxNums.pop()
-        const newProductOf3 = maxNums.reduce((a,b) => a*b) * arrayOfInts[i]
-        
-        if (newProductOf3 > currentBiggest) {
-          maxNums.push(arrayOfInts[i])
-          arrayOfInts[i] = originalNum
-        } else {
-          maxNums.push(originalNum)
-        }
-      }
-
-      maxNums.unshift(maxNums.pop())
+    let highestProductOf3 = arrayOfInts[0] * arrayOfInts[1] * arrayOfInts[2];
+    let highestProductOf2 = arrayOfInts[0] * arrayOfInts[1];
+    let lowestProductOf2  = arrayOfInts[0] * arrayOfInts[1];
+    let highest = Math.max(arrayOfInts[0], arrayOfInts[1]);
+    let lowest  = Math.min(arrayOfInts[0], arrayOfInts[1]);
+  
+    for (let i = 2; i < arrayOfInts.length; i++) {
+      const current = arrayOfInts[i];
+  
+      highestProductOf3 = Math.max(
+        highestProductOf3, current * highestProductOf2, current * lowestProductOf2
+      )
+      highestProductOf2 = Math.max(
+        highestProductOf2, current * highest, current * lowest
+      )
+      lowestProductOf2 = Math.min(
+        lowestProductOf2, current * highest, current * lowest
+      )
+      highest = Math.max(highest, current);
+      lowest = Math.min(lowest, current);
     }
   
-    return maxNums.reduce((a,b) => a*b)
+    return highestProductOf3;
 }
+
+// // Second Try Doesn't quite work
+// // Runtime O(n)
+// // Space O(1)
+// function highestProductOf3(arrayOfInts) {
+//     if (arrayOfInts.length < 3) {
+//         throw 'Less than 3 items!'
+//     }
+  
+//     let maxNums = [arrayOfInts.pop(), arrayOfInts.pop(), arrayOfInts.pop()]
+    
+//     for (let count = 0; count < 3; count++) {
+//         for (let i = 0; i < arrayOfInts.length; i++) {
+//             const currentBiggest = maxNums.reduce((a,b) => a*b)
+//             const originalNum = maxNums.pop()
+//             const newProductOf3 = maxNums.reduce((a,b) => a*b) * arrayOfInts[i]
+            
+//             if (newProductOf3 > currentBiggest) {
+//                 maxNums.push(arrayOfInts[i])
+//                 arrayOfInts[i] = originalNum
+//             } else {
+//                 maxNums.push(originalNum)
+//             }
+//         }
+
+//         maxNums.unshift(maxNums.pop())
+//     }
+  
+//     return maxNums.reduce((a,b) => a*b)
+// }
 
 // // First Try O(n)
 // // Doesn't work with negative numbers
 // function highestProductOf3(arrayOfInts) {
-//     if (arrayOfInts.length < 4) {
-//         throw 'error'
+//     if (arrayOfInts.length < 3) {
+//         throw 'Less than 3 items!'
 //     }
 
 //     const biggestNums = []
@@ -54,3 +85,7 @@ function highestProductOf3(arrayOfInts) {
 
 //     return biggestNums.reduce((a, b) => a * b)
 // }
+
+const arrayOfInts = [-10, -10, 4, 5, 6, 20, 12, -20, 2, 3, 6, 1, 0, 20, -6]
+console.log("INPUT:", arrayOfInts)
+console.log("RESULT:", highestProductOf3(arrayOfInts))
