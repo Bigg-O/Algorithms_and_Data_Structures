f7e85887f85504b23002363f1c1bd4d104811313
Wookeun Song <wookeun.s@gmail.com>
2020-04-09 16:46:03 -0500

    NP

---
+++ JavaScript/InterviewCake/3.Greedy_algorithms/In-Place_Shuffle.js
@@ -0,0 +1,9 @@
+function getRandom(floor, ceiling) {
+    return Math.floor(Math.random() * (ceiling - floor + 1)) + floor;
+}
+  
+function shuffle(array) {
+
+    // Shuffle the input in place
+
+}
