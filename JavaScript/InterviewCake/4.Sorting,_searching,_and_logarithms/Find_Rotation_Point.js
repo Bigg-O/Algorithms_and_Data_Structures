2f1efdcf960ff7d285f92c0b6d4885ab2621a7c1
Wookeun Song <wookeun.s@gmail.com>
2020-04-09 19:10:33 -0500

    Third Solution

--- JavaScript/InterviewCake/4.Sorting,_searching,_and_logarithms/Find_Rotation_Point.js
+++ JavaScript/InterviewCake/4.Sorting,_searching,_and_logarithms/Find_Rotation_Point.js
@@ -1,3 +1,24 @@
+// Third Recursive Solution
+// Runtime: O(log(n))
+// Space: O(1)
+function findRotationPoint(words, start = 0, end = words.length - 1) {
+    const mid = Math.floor((start + end) / 2)
+  
+    if (mid === words.length) {
+      return 0    
+    } else if (mid === 0)  {
+      return words.length - 1
+    } else if (words[mid-1] > words[mid]) {
+      return mid
+    }
+    
+    if (words[0] > words[mid]) {
+      return findRotationPoint(words, start, mid-1)
+    } else {
+      return findRotationPoint(words, mid+1, end)
+    }
+}
+
 // Second Solution
 // Runtime:
 //     best: O(1)
