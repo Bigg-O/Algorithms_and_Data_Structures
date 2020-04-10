ba8b9b816944263c3f8bb6413838a304255df481
Wookeun Song <wookeun.s@gmail.com>
2020-04-10 17:40:00 -0500

    first solution

--- JavaScript/InterviewCake/4.Sorting,_searching,_and_logarithms/Find_Repear,_Space_Edition.js
+++ JavaScript/InterviewCake/4.Sorting,_searching,_and_logarithms/Find_Repear,_Space_Edition.js
@@ -1,7 +1,18 @@
+// Second Solution Optimizing for space
 function findRepeat(numbers) {
 
-    // Find a number that appears more than once
-    
-  
-    return 0;
+}
+
+// First Solution Optimizing for runtime
+// Runtime: O(n)
+// Space: O(n)
+function findRepeat(numbers) {
+    const numSet = new Set()
+
+    for (const num of numbers) {
+        if (numSet.has(num)) return num
+        else numSet.add(num)
+    }
+
+    return false
 }
