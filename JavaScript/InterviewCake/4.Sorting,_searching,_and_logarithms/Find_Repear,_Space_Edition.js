a45d30767c004f954845a58d392006e370b50beb
Wookeun Song <wookeun.s@gmail.com>
2020-04-10 22:03:55 -0500

    lil fix

--- JavaScript/InterviewCake/4.Sorting,_searching,_and_logarithms/Find_Repear,_Space_Edition.js
+++ JavaScript/InterviewCake/4.Sorting,_searching,_and_logarithms/Find_Repear,_Space_Edition.js
@@ -1,28 +1,35 @@
-// Second Solution Optimizing for space
+// Third Solution Optimizing for space (Immutable)
 // Runtime: O(n*log(n))
 // Space: O(1)
 function findRepeat(numbers) {
-    numbers = numbers.sort((a,b) => a-b)
 
-    for (let i = 0; i < numbers.length - 1; i++) {
-      if (numbers[i] === numbers[i+1]) {
-        return numbers[i]
-      }
+}
+
+// Second Solution with sorting
+// Runtime: O(n*log(n))
+// Space: O(n*log(n))
+function findRepeat(numbers) {
+  numbers = numbers.sort((a,b) => a-b)
+
+  for (let i = 0; i < numbers.length - 1; i++) {
+    if (numbers[i] === numbers[i+1]) {
+      return numbers[i]
     }
-    
-    return false
+  }
+  
+  return false
 }
 
 // First Solution Optimizing for runtime
 // Runtime: O(n)
 // Space: O(n)
 function findRepeat(numbers) {
-    const numSet = new Set()
+  const numSet = new Set()
 
-    for (const num of numbers) {
-        if (numSet.has(num)) return num
-        else numSet.add(num)
-    }
+  for (const num of numbers) {
+    if (numSet.has(num)) return num
+    else numSet.add(num)
+  }
 
-    return false
+  return false
 }
