d31851ee052fee1e1d0ee3e5e15ccec4e220480d
Wookeun Song <wookeun.s@gmail.com>
2020-04-13 00:01:35 -0500

    second solution

--- JavaScript/InterviewCake/5.Trees_and_graphs/2nd_Largest_Item_in_a_Binary_Search_Tree.js
+++ JavaScript/InterviewCake/5.Trees_and_graphs/2nd_Largest_Item_in_a_Binary_Search_Tree.js
@@ -16,8 +16,32 @@ class BinaryTreeNode {
     }
 }
   
-// First Try with help of Medium.
+// Second Solution
+// Post Order Traverse until length = 2
+// Runtime: O(h), h = height of the tree
+// If BST is balanced h = log(n), else h = n
+// Space: O(h) recursive call stack
+function findSecondLargest(treeRoot) {
+	const nodes = []
+	postOrderTraverse(treeRoot, nodes)
+	
+	if (nodes.length > 1) return nodes[1]
+    else throw 'error'
+}
+
+function postOrderTraverse(node, nodes) {
+    if (nodes.length === 2) return;
+    if (node) {
+        postOrderTraverse(node.right, nodes);
+        nodes.push(node.value)
+        postOrderTraverse(node.left, nodes);
+    }
+}
+
+// First Solution with help of Medium.
 // In Order Traverse to get sorted array.
+// Runtime: O(n)
+// Space: O(n)
 function findSecondLargest(treeRoot) {
 	const nodes = []
 	inOrderTraverse(treeRoot, nodes)
