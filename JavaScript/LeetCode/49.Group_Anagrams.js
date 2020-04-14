7ff87b250f0a129111eda8856a591eed93ec8094
Wookeun Song <wookeun.s@gmail.com>
2020-04-14 00:55:09 -0500

    SOLUTION

--- JavaScript/LeetCode/49.Group_Anagrams.js
+++ JavaScript/LeetCode/49.Group_Anagrams.js
@@ -1,3 +1,18 @@
+// First Solution
+// Runtime: O(n*(w*log(w))) w = length of the word
+// Space: O(n)
 var groupAnagrams = function(strs) {
+    const anagrams = {}
     
+    for (const word of strs) {
+        const sortedWord = word.split('').sort().join('')
+        
+        if (anagrams[sortedWord]) {
+            anagrams[sortedWord].push(word)
+        } else {
+            anagrams[sortedWord] = [word]
+        }
+    }
+    
+    return Object.values(anagrams)
 };
