4508c0e2dc63a2f80d4e719df4f636419102ecac
Wookeun Song <wookeun.s@gmail.com>
2020-04-17 23:50:19 -0500

    In Progress

--- JavaScript/HackerRank/Interview_Prep_Kit/Dictionaries_and_Hashmaps/Count_Priplets.js
+++ JavaScript/HackerRank/Interview_Prep_Kit/Dictionaries_and_Hashmaps/Count_Priplets.js
@@ -1,3 +1,15 @@
+// First Try In Progress
 function countTriplets(arr, r) {
+    let result = 0
+    const numMap = {}
+    const min = arr[0]
+    const max = arr[arr.length-1]
 
+    for (const num of arr) {
+        numMap[num] = (numMap[num] || 0) + 1
+    }
+
+    console.log(numMap)
+
+    return r
 }
