06585a942203dc278f7c0475d7c0b77ce0b02151
Wookeun Song <wookeun.s@gmail.com>
2020-04-20 23:00:22 -0500

    second solution

--- JavaScript/LeetCode/69.Sqrt(x).js
+++ JavaScript/LeetCode/69.Sqrt(x).js
@@ -1,3 +1,18 @@
+// Second Solution
+var mySqrt = function(x) {
+    let result = 0
+    
+    while (result * result <= x) {
+        if (result * result === x) {
+            return result
+        } else {
+            result++
+        }
+    }
+    
+    return result - 1
+};
+
 // First Solution
 var mySqrt = function(x) {
     let result = 0
