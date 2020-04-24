b8882d87a535ccda49a066696ccb605c132a2c8c
Wookeun Song <wookeun.s@gmail.com>
2020-04-23 22:49:30 -0500

    solved

--- JavaScript/LeetCode/136.Single_Number.js
+++ JavaScript/LeetCode/136.Single_Number.js
@@ -1,3 +1,16 @@
+// First Try using Set
+// Runtime: O(n)
+// Space: O(1) ???
 var singleNumber = function(nums) {
+    const numSet = new Set()
     
+    for (const num of nums) {
+        if (numSet.has(num)) {
+            numSet.delete(num)
+        } else {
+            numSet.add(num)
+        }
+    }
+    
+    return [...numSet][0]
 }
