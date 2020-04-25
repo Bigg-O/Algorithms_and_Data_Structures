fdbf824395180c54262787fffca908a9cacb545a
Wookeun Song <wookeun.s@gmail.com>
2020-04-24 21:38:23 -0500

    second solution

--- JavaScript/LeetCode/136.Single_Number.js
+++ JavaScript/LeetCode/136.Single_Number.js
@@ -1,6 +1,16 @@
-// First Try using Set
+// Second solution using sets and math
 // Runtime: O(n)
-// Space: O(1) ???
+// Space: O(n)
+var singleNumber = function(nums) {
+    const uniqNums = [...new Set(nums)]
+    const uniqSum = uniqNums.reduce((a,b) => a+b)
+    const sum = nums.reduce((a,b) => a+b)
+    return uniqSum * 2 - sum
+};
+
+// First try using Set
+// Runtime: O(n)
+// Space: O(n)
 var singleNumber = function(nums) {
     const numSet = new Set()
     
