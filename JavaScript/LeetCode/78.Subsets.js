ac4b46ecc14da2facfd2bc1c18dfb5f36cc369e7
Wookeun Song <wookeun.s@gmail.com>
2020-05-04 01:03:15 -0500

    so close

--- JavaScript/LeetCode/78.Subsets.js
+++ JavaScript/LeetCode/78.Subsets.js
@@ -1,17 +1,17 @@
 function subsets(nums) {
-    const result = []
+    const result = [[]]
 
-    for (let i = 1; i <= nums.length; i++) {
+    nums.forEach(num => result.push([num]))
+
+    for (let i = 1; i < nums.length; i++) {
         for (let j = 0; j < nums.length; j++) {
-            const current = nums.slice(j,i)
+            const current = nums.slice(j,j+i)
     
-            for (let k = j; k < nums.length; k++) {
+            for (let k = j+i; k < nums.length; k++) {
                 result.push([...current, nums[k]])
             }
         }
-        console.log("line",i, result)
     }
-
 
     return result
 }
@@ -19,3 +19,6 @@ function subsets(nums) {
 const INPUT = [1,2,3,4]
 console.log("INPUT:", INPUT)
 console.log("OUTPUT:", subsets(INPUT))
+
+const ex = [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3],[4],[1,4],[2,4],[1,2,4],[3,4],[1,3,4],[2,3,4],[1,2,3,4]]
+console.log(ex)
