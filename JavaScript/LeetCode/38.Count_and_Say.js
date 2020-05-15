eeb0b3cbc534742845f800709311e96301d5e17b
Wookeun Song <wookeun.s@gmail.com>
2020-05-15 11:19:26 -0500

    leetcode added

---
+++ JavaScript/LeetCode/38.Count_and_Say.js
@@ -0,0 +1,31 @@
+// Second Attempt
+// Recursive Solution
+var countAndSay = function(n) {
+
+}
+
+// First Attempt
+// Iterative Solution
+var countAndSay = function(n) {
+    let result = [1]
+    
+    for (let i = 1; i < n; i++) {
+        const currentCode = result
+        result = []
+        
+        let count = 1
+        while (currentCode.length) {
+            let currentChar = currentCode.shift()
+            
+            while (currentChar === currentCode[0]) {
+                currentCode.shift()
+                count++
+            }
+            
+            result.push(count, currentChar)
+            count = 1
+        }
+    }
+    
+    return result.join('')
+}
