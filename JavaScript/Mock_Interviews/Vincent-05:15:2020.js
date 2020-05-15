36e205b4d8c22f361ed3419924bf99dfeaddc72f
Wookeun Song <wookeun.s@gmail.com>
2020-05-15 11:01:26 -0500

    first attempt

---
+++ JavaScript/Mock_Interviews/Vincent-05:15:2020.js
@@ -0,0 +1,27 @@
+// Interviewer : Vincent Marks
+// Interviewee : Wookeun Song
+// Time : 10AM
+
+var countAndSay = function(n) {
+    let result = [1]
+    
+    for (let i = 1; i < n; i++) {
+        const currentCode = result
+        result = []
+        
+        let count = 1
+        while (currentCode.length) {
+            let currentChar = currentCode.shift()
+            
+            while (currentChar === currentCode[0]) {
+                currentCode.shift()
+                count++
+            }
+            
+            result.push(count, currentChar)
+            count = 1
+        }
+    }
+    
+    return result.join('')
+}
