5e040ea7130e66328b9f2d45ea6e448f4b7b6bdb
Wookeun Song <wookeun.s@gmail.com>
2020-06-26 23:29:43 +0900

    FS

--- JavaScript/Other/how_many_smalls.js
+++ JavaScript/Other/how_many_smalls.js
@@ -4,15 +4,25 @@
 // Return number of least smalls that can be used to meet the goal
 // If it can't be done return -1
 
-function countSmalls (goal, small, big) {
-    
+// First Solution
+function countSmalls (goal, big, small) {
+    while (goal >= 5 && big) {
+        goal -= 5
+        big--
+    }
+
+    goal -= small
+
+    if (goal === 0) return small
+    else if (goal < 0) return small + goal
+    else return -1
 }
 
-const goal = 15
-const small = 3
-const big = 3
+const goal = 18
+const big = 2
+const small = 4
 
 console.log("goal:", goal)
-console.log("small:", small)
 console.log("big:", big)
-console.log("RESULT:", countSmalls(goal, small, big))
+console.log("small:", small)
+console.log("RESULT:", countSmalls(goal, big, small))
