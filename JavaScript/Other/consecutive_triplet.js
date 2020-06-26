64651d30ff17d71d2bf28ae50d0fbcb94d92b995
Wookeun Song <wookeun.s@gmail.com>
2020-06-27 00:06:44 +0900

    first solution

--- JavaScript/Other/consecutive_triplet.js
+++ JavaScript/Other/consecutive_triplet.js
@@ -3,9 +3,17 @@
 // ex) [1,2,3] => true
 
 function findConsecutiveTriplet(arr) {
+    if (arr.length < 3) return false
 
+    for (let i = 0; i < arr.length - 2; i++) {
+        if (arr[i]+2 === arr[i+1]+1 && arr[i+1]+1 === arr[i+2]) {
+            return true
+        }
+    }
+
+    return false
 }
 
-const arr = [1,2,2,3,4,5]
+const arr = [1,2,5,3,4,5]
 console.log("arr:", arr)
-console.log("RESULT:", findTriplet(arr))
+console.log("RESULT:", findConsecutiveTriplet(arr))
