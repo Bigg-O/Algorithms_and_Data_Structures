ca1fbc1500a105897bfac865526acc7422744d6b
Wookeun Song <wookeun.s@gmail.com>
2020-06-26 23:21:01 +0900

    how many smalls base set up

--- JavaScript/Other/how_many_smalls.js
+++ JavaScript/Other/how_many_smalls.js
@@ -1,5 +1,18 @@
 // iFit technical challenge
+// big = 5
+// small = 1
+// Return number of least smalls that can be used to meet the goal
+// If it can't be done return -1
 
 function countSmalls (goal, small, big) {
     
 }
+
+const goal = 15
+const small = 3
+const big = 3
+
+console.log("goal:", goal)
+console.log("small:", small)
+console.log("big:", big)
+console.log("RESULT:", countSmalls(goal, small, big))
