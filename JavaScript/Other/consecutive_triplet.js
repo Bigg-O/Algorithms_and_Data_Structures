85f509c29a525d4a7b6c62c1a783b1d7662e5edb
Wookeun Song <wookeun.s@gmail.com>
2020-06-26 23:57:50 +0900

    new problem triplet

---
+++ JavaScript/Other/consecutive_triplet.js
@@ -0,0 +1,11 @@
+// iFit technical challenge
+// This function returns true if the inut array has a consecutive triplet
+// ex) [1,2,3] => true
+
+function findConsecutiveTriplet(arr) {
+
+}
+
+const arr = [1,2,2,3,4,5]
+console.log("arr:", arr)
+console.log("RESULT:", findTriplet(arr))
