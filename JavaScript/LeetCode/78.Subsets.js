01bc7dcc9693b67169677a72a96be7c4b9b3da75
Wookeun Song <wookeun.s@gmail.com>
2020-07-11 21:42:18 +0900

    close

--- JavaScript/LeetCode/78.Subsets.js
+++ JavaScript/LeetCode/78.Subsets.js
@@ -1,12 +1,19 @@
+
+// Pretty damn close 
+// Can't cant 1,3,4
 function subsets(nums) {
     const result = [[]]
+    nums.forEach(num => {result.push([num])})
 
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = 1; j < nums.length; j++) {
-
+    for (let count = 1; count <= nums.length; count++) {
+        for (let start = 0; start < nums.length - count; start++) {
+            for (let i = count + start; i < nums.length; i++) {
+                result.push([...nums.slice(start, start + count), nums[i]])
+            }
         }
     }
 
+    return result
 }
 
 const INPUT = [1,2,3,4]
