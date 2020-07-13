2c239addcf92f3edef21bf553ab4b55f5f061678
Wookeun Song <wookeun.s@gmail.com>
2020-07-13 19:54:41 +0900

    using sets

--- JavaScript/LeetCode/78.Subsets.js
+++ JavaScript/LeetCode/78.Subsets.js
@@ -1,21 +1,22 @@
 
-// Pretty damn close 
-// Can't cant 1,3,4
+// First Attempt
+// Brute Force
 function subsets(nums) {
-    const result = [[]]
-    nums.forEach(num => {result.push([num])})
+    const result = new Set()
+    result.add([])
+    nums.forEach(num => result.add([num]))
 
     for (let count = 1; count <= nums.length; count++) {
         for (let start = 0; start < nums.length - count; start++) {
-
-            
-            for (let i = count + start; i < nums.length; i++) {
-                result.push([...nums.slice(start, start + count), nums[i]])
+            for (let i = 0; i < nums.length; i++) {
+                if (i < start || i > start + count) {
+                    result.add([...nums.slice(start, start + count), nums[i]].sort())
+                }
             }
         }
     }
 
-    return result
+    return [...result]
 }
 
 const INPUT = [1,2,3,4]
