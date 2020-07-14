487c31d2a925eeb6ccac47fa17244803a2160dc3
Wookeun Song <wookeun.s@gmail.com>
2020-07-14 16:53:39 +0900

    back to simple array methods

--- JavaScript/LeetCode/78.Subsets.js
+++ JavaScript/LeetCode/78.Subsets.js
@@ -2,21 +2,20 @@
 // First Attempt
 // Brute Force
 function subsets(nums) {
-    const result = new Set()
-    result.add([])
-    nums.forEach(num => result.add([num]))
+    const result = [[]]
+    nums.forEach(num => {result.push([num])})
 
     for (let count = 1; count <= nums.length; count++) {
         for (let start = 0; start < nums.length - count; start++) {
-            for (let i = 0; i < nums.length; i++) {
-                if (i < start || i > start + count) {
-                    result.add([...nums.slice(start, start + count), nums[i]].sort())
-                }
+            for (let i = count + start; i < nums.length; i++) {
+                result.push([...nums.slice(start, start + count), nums[i]])
             }
         }
     }
 
-    return [...result]
+    return result
+
+    
 }
 
 const INPUT = [1,2,3,4]
