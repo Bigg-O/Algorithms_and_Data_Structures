4cf86e15b5b9810e498ed83bc0b4606aacdf3e07
Wookeun Song <wookeun.s@gmail.com>
2020-07-18 17:17:11 +0900

    pretty close

--- JavaScript/HackerRank/Interview_Prep_Kit/Dictionaries_and_Hashmaps/Count_Triplets.js
+++ JavaScript/HackerRank/Interview_Prep_Kit/Dictionaries_and_Hashmaps/Count_Triplets.js
@@ -1,26 +1,42 @@
-// // Second Try
-// function countTriplets(arr, r) {
-
-// }
-
-// First Try
+// Second Try
 function countTriplets(arr, r) {
-    arr = arr.filter(num => !(num % r) || num === 1)
+    let result = 0
     const numMap = {}
     for (const num of arr) {
-        numMap[num] = (numMap[num] || 0) + 1
+        if (numMap[num]) numMap[num]++
+        else numMap[num] = 1
     }
-    let result = 0
-    const min = arr[0]
-    const max = arr[arr.length-1]
 
-    console.log(numMap)
+    for (const num of arr) {
+        if (numMap[num * r] && numMap[num * r * r]) {
+            result++
+            if (2 < numMap[num * r] + numMap[num * r * r]) {
+                result += numMap[num * r] + numMap[num * r * r] - 2
+            }
+        }
+    }
 
     return result
 }
 
-const arr = [1,4,16,64]
-const r = 4
+// // Incomplete First Try
+// function countTriplets(arr, r) {
+//     arr = arr.filter(num => !(num % r) || num === 1)
+//     const numMap = {}
+//     for (const num of arr) {
+//         numMap[num] = (numMap[num] || 0) + 1
+//     }
+//     let result = 0
+//     const min = arr[0]
+//     const max = arr[arr.length-1]
+
+//     console.log(numMap)
+
+//     return result
+// }
+
+const arr = [1,3,9,9,27,81]
+const r = 3
 console.log("arr:", arr)
 console.log("r:", r)
 console.log("OUTPUT:", countTriplets(arr, r))
