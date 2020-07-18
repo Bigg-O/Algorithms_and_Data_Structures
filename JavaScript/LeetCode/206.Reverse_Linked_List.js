9d0b36b213ebdc1c23bd0ebb94fd144fc0332bde
Wookeun Song <wookeun.s@gmail.com>
2020-07-18 18:56:21 +0900

    First Iterative solution

--- JavaScript/LeetCode/206.Reverse_Linked_List.js
+++ JavaScript/LeetCode/206.Reverse_Linked_List.js
@@ -1,3 +1,19 @@
+// Fisrt Iterative Solution
 var reverseList = function(head) {
+    let node = head
+    const tempArr = []
+    while (node) {
+        tempArr.unshift(node.val)
+        node = node.next
+    }
     
+    const result = new ListNode(null)
+    node = result
+    for (const nodeVal of tempArr) {
+        node.next = new ListNode(nodeVal)
+        node = node.next
+    }
+    
+    node.next = null
+    return result.next
 };
