f1a83062f87483447222ddad5b542a1d0d6f2ced
Wookeun Song <wookeun.s@gmail.com>
2020-07-21 19:00:54 +0900

    one more

---
+++ JavaScript/FB/Topic1-Arrays&Strings/Left_Rotation.js
@@ -0,0 +1,12 @@
+// Runtime Complexity:
+//    O(n), n = arr.length
+// Space Complexity:
+//    O(1)
+function rotLeft(arr, num) {
+    num %= arr.length
+    for (let i = 0; i < num; i++) {
+        arr.push(arr.shift())
+    }
+    
+    return arr
+}
