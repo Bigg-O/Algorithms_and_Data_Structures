219a9c54a6265020e4ad9f545e2add740007757b
Wookeun Song <wookeun.s@gmail.com>
2020-07-21 18:47:15 +0900

    finished

--- JavaScript/FB/Topic1-Arrays&Strings/Designer_PDF_Viewer.js
+++ JavaScript/FB/Topic1-Arrays&Strings/Designer_PDF_Viewer.js
@@ -1,4 +1,15 @@
-function designerPdfViewer(h, word) {
+// Runtime Complexity:
+//    O(n)
+// Space Complexity:
+//    O(1)
+function designerPdfViewer(heights, word) {
+    let max = 0
+    for (const char of word) {
+        const charIndex = char.charCodeAt(0) - 97
+        if (heights[charIndex] > max) {
+            max = heights[charIndex]
+        }
+    }
 
-
+    return max * word.length
 }
