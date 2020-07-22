42f0afd3166537f46fae685c8aba168cd69c2c2c
Wookeun Song <wookeun.s@gmail.com>
2020-07-22 15:02:58 +0900

    done

--- JavaScript/FB/Topic3-Stacks&Queues/Queue_using_Two_Stacks.js
+++ JavaScript/FB/Topic3-Stacks&Queues/Queue_using_Two_Stacks.js
@@ -2,9 +2,21 @@
 // https://www.hackerrank.com/challenges/queue-using-two-stacks/problem
 
 // Runtime Complexity:
-//    O(
+//    O(n)
 // Space Complexity:
-//    O(
+//    O(n)
 function processData(input) {
+    const queue = []
+    const inputs = input.split("\n")
+    inputs.shift()
 
+    for (const input of inputs) {
+        if (input[0] == 1) {
+            queue.push(input.split(' ')[1])
+        } else if (input[0] == 2) {
+            queue.shift()
+        } else {
+            console.log(queue[0])
+        }
+    }
 }
