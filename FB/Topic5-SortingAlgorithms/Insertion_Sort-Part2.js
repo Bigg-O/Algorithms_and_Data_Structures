c743abfddccb4de6073b59a37d98def4b7c11bf2
Wookeun Song <wookeun.s@gmail.com>
2020-07-22 20:40:39 +0900

    insertion sort

--- FB/Topic5-SortingAlgorithms/Insertion_Sort-Part2.js
+++ FB/Topic5-SortingAlgorithms/Insertion_Sort-Part2.js
@@ -2,10 +2,20 @@
 // https://www.hackerrank.com/challenges/insertionsort2/problem
 
 // Runtime Complexity:
-//    O(
+//    O(n^2)
 // Space Complexity:
-//    O(
+//    O(1)
 function insertionSort2(n, arr) {
+    for (let i = 1; i < arr.length; i++) {
 
-
+        let j = i
+        while (arr[j - 1] > arr[j]) {
+          const temp = arr[j - 1]
+          arr[j - 1] = arr[j]
+          arr[j] = temp
+          j--
+        }
+        
+        process.stdout.write(arr.join(' ') + '\n')
+    }
 }
