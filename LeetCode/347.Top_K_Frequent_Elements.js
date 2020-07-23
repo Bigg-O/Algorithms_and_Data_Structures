fbca0a9ba4d6a5e02fff0094e9684b73a5de0796
Wookeun Song <wookeun.s@gmail.com>
2020-07-23 22:39:17 +0900

    In the process

--- LeetCode/347.Top_K_Frequent_Elements.js
+++ LeetCode/347.Top_K_Frequent_Elements.js
@@ -6,5 +6,16 @@
 // Space Complexity:
 //    
 var topKFrequent = function(nums, k) {
-    
+    const frequencyMap = {}
+    for (const num of nums) {
+      frequencyMap[num] = frequencyMap[num] + 1 || 1
+    }
+
+  
 };
+
+const nums = [1,1,1,2,2,3]
+const k = 2
+console.log("nums:", nums)
+console.log("k:", k)
+console.log("OUTPUT:", topKFrequent(nums, k))
