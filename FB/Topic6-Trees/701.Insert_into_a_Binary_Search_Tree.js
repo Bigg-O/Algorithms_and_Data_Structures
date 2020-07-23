3ea05af16f854d74f6e3088290d40262ed78ede1
Wookeun Song <wookeun.s@gmail.com>
2020-07-23 13:35:32 +0900

    done

--- FB/Topic6-Trees/701.Insert_into_a_Binary_Search_Tree.js
+++ FB/Topic6-Trees/701.Insert_into_a_Binary_Search_Tree.js
@@ -1,6 +1,25 @@
 // 701. Insert into a Binary Search Tree
 // https://leetcode.com/problems/insert-into-a-binary-search-tree/
 
-var insertIntoBST = function(root, val) {
+const insertIntoBST = function(root, val, node = root) {
+    if (!root) {
+      root = new TreeNode(val)
+      return root
+    }
     
+    if (val < node.val) {
+      if (!node.left) {
+        node.left = new TreeNode(val)
+        return root
+      }
+      node = node.left
+    } else if (val > node.val) {
+      if (!node.right) {
+        node.right = new TreeNode(val)
+        return root
+      }
+      node = node.right
+    }
+    
+    return insertIntoBST(root, val, node)
 };
