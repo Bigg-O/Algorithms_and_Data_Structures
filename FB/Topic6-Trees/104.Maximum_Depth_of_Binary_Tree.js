21350a52085f97095c1663f457c9387eda420519
Wookeun Song <wookeun.s@gmail.com>
2020-07-23 17:52:40 +0900

    TREE

--- FB/Topic6-Trees/104.Maximum_Depth_of_Binary_Tree.js
+++ FB/Topic6-Trees/104.Maximum_Depth_of_Binary_Tree.js
@@ -1,10 +1,21 @@
 // 104. Maximum Depth of Binary Tree
 // https://leetcode.com/problems/maximum-depth-of-binary-tree/
 
+// Leetcode best solution
 // Runtime Complexity:
-//    O(
+//    O(n) n = depth of the tree
 // Space Complexity:
-//    O()
+//    O(1)
 var maxDepth = function(root) {
-    
+    if (root === null) return 0
+    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1
+};
+
+// Runtime Complexity:
+//    O(n) n = depth of the tree
+// Space Complexity:
+//    O(1)
+var maxDepth = function (root, depth = 0) {
+    if (!root) return depth
+    return Math.max(maxDepth(root.left, depth + 1), maxDepth(root.right, depth + 1))
 };
--- FB/Topic6-Trees/701.Insert_into_a_Binary_Search_Tree.js
+++ FB/Topic6-Trees/701.Insert_into_a_Binary_Search_Tree.js
@@ -1,14 +1,31 @@
 // 701. Insert into a Binary Search Tree
 // https://leetcode.com/problems/insert-into-a-binary-search-tree/
 
+// Leetcode best solution
+// Runtime Complexity:
+//    O(n) n = depths of BST
+// Space Complexity:
+//    O(1)
+var insertIntoBST = function(root, val) {
+  if(!root) {
+      return new TreeNode(val);
+  }
+  if(val > root.val){
+      root.right = insertIntoBST(root.right, val);
+  }
+  else {
+      root.left = insertIntoBST(root.left, val);
+  }
+  return root;
+};
+
 // Runtime Complexity:
 //    O(n) n = depths of BST
 // Space Complexity:
 //    O(1)
 const insertIntoBST = function(root, val, node = root) {
     if (!root) {
-      root = new TreeNode(val)
-      return root
+      return new TreeNode(val)
     }
     
     if (val < node.val) {
