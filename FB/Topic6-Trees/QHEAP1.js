b983e088c39c48d421aabb7c89bdd1275771c9e8
Wookeun Song <wookeun.s@gmail.com>
2020-07-23 17:56:51 +0900

    NP

---
+++ FB/Topic6-Trees/QHEAP1.js
@@ -0,0 +1,10 @@
+// QHEAP1
+// https://www.hackerrank.com/challenges/qheap1/problem?isFullScreen=false
+
+// Runtime Complexity:
+//    O(
+// Space Complexity:
+//    O(
+function processData(input) {
+    //Enter your code here
+}
