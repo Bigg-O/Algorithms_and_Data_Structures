f0a39423ce11863a3a0276cdb62930efaedbed5f
Wookeun Song <wookeun.s@gmail.com>
2020-10-04 16:03:47 -0500

    redo

---
+++ HackerRank/Interview_Prep_Kit/Warm-up Challenges/Sales_by_Match.js
@@ -0,0 +1,23 @@
+// 10/04/2020
+// Redo
+function sockMerchant(n, ar) {
+
+}
+
+// My previous code
+// function sockMerchant(n, ar) {
+//     const obj = {}
+//     for (const val of ar) {
+//         if (obj[val]) {
+//             obj[val]++
+//         } else {
+//             obj[val] = 1
+//         }
+//     }
+
+//     let result = 0
+//     for (const val in obj){
+//         result += Math.floor(obj[val] / 2)
+//     }
+//     return result
+// }
