371f3fb06566e6a62f553be8280d92654c0cf95a
Wookeun Song <wookeun.s@gmail.com>
2020-10-07 15:56:50 -0500

    NP

---
+++ HackerRank/Interview_Prep_Kit/Warm-up Challenges/Counting_Valleys.js
@@ -0,0 +1,64 @@
+// https://www.hackerrank.com/challenges/counting-valleys/problem?h_l=interview&playlist_slugs%5B%5D%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D%5B%5D=warmup&isFullScreen=true
+
+// 10/07/2020
+// redo
+// Runtime : 
+// Space : 
+function countingValleys(num, str) {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Previous code
+// function countingValleys(num, str) {
+//     let valleyCount = 0
+//     let stepCount = 0
+//     for (let i = 0; i < num; i++) {
+//         if (str[i] === "U") {
+//             stepCount++
+//             if (stepCount === 0) {
+//                 valleyCount++
+//             }
+//         } else if(str[i] === "D") {
+//             stepCount--
+//         }
+//     }
+//     return valleyCount
+// }
