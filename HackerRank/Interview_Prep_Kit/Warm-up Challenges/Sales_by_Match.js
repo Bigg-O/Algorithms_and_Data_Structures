b1d73bc0fa7f446860a968a2d0ab6e833eefa901
Wookeun Song <wookeun.s@gmail.com>
2020-10-07 15:54:23 -0500

    fin

--- HackerRank/Interview_Prep_Kit/Warm-up Challenges/Sales_by_Match.js
+++ HackerRank/Interview_Prep_Kit/Warm-up Challenges/Sales_by_Match.js
@@ -1,9 +1,22 @@
 // https://www.hackerrank.com/challenges/sock-merchant/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
 
-// 10/04/2020
+// 10/07/2020
 // Redo
+// Runtime : O(n)
+// Space : O(n)
 function sockMerchant(n, ar) {
+    const obj = {}
+    for (const i of ar) {
+        if (obj[i]) obj[i]++
+        else obj[i] = 1
+    }
 
+    let result = 0
+    Object.values(obj).forEach( num => {
+        if (num >= 2) result += Math.floor(num / 2)
+    })
+
+    return result
 }
 
 // My previous code
