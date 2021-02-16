3265f53a9f4f160932dcafe9b53702671a1f23d5
Wookeun Song <wookeun.s@gmail.com>
2021-02-16 00:35:56 -0600

    mid change

--- InterviewCake/1.Array_and_string_manipulation/Merging_Meeting_Times.js
+++ InterviewCake/1.Array_and_string_manipulation/Merging_Meeting_Times.js
@@ -1,32 +1,80 @@
-// Indestructive Solution
-function mergeRanges(meetings) {
-  const sortedMeetings = [...meetings.sort((a, b) => a.startTime - b.startTime)]
+// Attempt 2/15/2021
+function mergeRanges(scheduleArr) {
+  const sortedSchedules = scheduleArr.sort(a.startTime - b.startTime)
+  console.log(sortedSchedules)
 
-  let i = 0
-  while (i < sortedMeetings.length - 1) {
-    // current End >= next Start
-    if (sortedMeetings[i].endTime >= sortedMeetings[i + 1].startTime) {
-      // current End < next End
-      if (sortedMeetings[i].endTime < sortedMeetings[i + 1].endTime) {
-        sortedMeetings[i].endTime = sortedMeetings[i + 1].endTime
-      }
-      sortedMeetings.splice(i + 1, 1)
-    } else {
-      i++
-    }
-  }
+  const result = [scheduleArr[0]]
 
-  return meetings;
+  // for (let i = 1; i < scheduleArr.length; i++) {
+  //   const current = scheduleArr[i]
+  //   if (current.startTime < result)
+  // }
 }
 
-const meetings = [{ startTime: 0, endTime: 1 },
-{ startTime: 3, endTime: 5 },
-{ startTime: 4, endTime: 8 },
-{ startTime: 10, endTime: 12 },
-{ startTime: 9, endTime: 10 }]
 
-console.log("meetings:", meetings)
-console.log("OUTPUT:", mergeRanges(meetings))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Indestructive Solution
+// function mergeRanges(meetings) {
+//   const sortedMeetings = [...meetings.sort((a, b) => a.startTime - b.startTime)]
+
+//   let i = 0
+//   while (i < sortedMeetings.length - 1) {
+//     // current End >= next Start
+//     if (sortedMeetings[i].endTime >= sortedMeetings[i + 1].startTime) {
+//       // current End < next End
+//       if (sortedMeetings[i].endTime < sortedMeetings[i + 1].endTime) {
+//         sortedMeetings[i].endTime = sortedMeetings[i + 1].endTime
+//       }
+//       sortedMeetings.splice(i + 1, 1)
+//     } else {
+//       i++
+//     }
+//   }
+
+//   return meetings;
+// }
+
+// const meetings = [{ startTime: 0, endTime: 1 },
+// { startTime: 3, endTime: 5 },
+// { startTime: 4, endTime: 8 },
+// { startTime: 10, endTime: 12 },
+// { startTime: 9, endTime: 10 }]
+
+// console.log("meetings:", meetings)
+// console.log("OUTPUT:", mergeRanges(meetings))
 
 // // InterviewCake Solution
 // function mergeRanges(meetings) {
