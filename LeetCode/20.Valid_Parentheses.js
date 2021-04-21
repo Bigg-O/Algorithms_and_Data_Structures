081f373ac944f10684e51527f561779f9d8d8362
Wookeun Song <wookeun.s@gmail.com>
2021-04-21 09:41:37 -0500

    paren

--- LeetCode/20.Valid_Parentheses.js
+++ LeetCode/20.Valid_Parentheses.js
@@ -64,38 +64,61 @@
 //     if (str === '}') return '{'
 // }
 
-// Third Solution
-const opens = {
-    '(': 1,
-    '{': 2,
-    '[': 3
-}
-const closes = {
-    ')': 1,
-    '}': 2,
-    ']': 3
-}
-var isValid = function (str) {
-    if (str.length % 2 === 1) {
-        return false
-    }
+// // Third Solution
+// const opens = {
+//     '(': 1,
+//     '{': 2,
+//     '[': 3
+// }
+// const closes = {
+//     ')': 1,
+//     '}': 2,
+//     ']': 3
+// }
+// var isValid = function (str) {
+//     if (str.length % 2 === 1) {
+//         return false
+//     }
 
-    const stack = []
+//     const stack = []
 
-    for (const char of str) {
-        if (opens[char]) {
-            stack.push(char)
-        } else if (stack.length && closes[char]) {
-            const stackPeak = stack[stack.length - 1]
-            if (opens[stackPeak] === closes[char]) {
-                stack.pop()
-            } else {
-                return false
-            }
-        } else {
+//     for (const char of str) {
+//         if (opens[char]) {
+//             stack.push(char)
+//         } else if (stack.length && closes[char]) {
+//             const stackPeak = stack[stack.length - 1]
+//             if (opens[stackPeak] === closes[char]) {
+//                 stack.pop()
+//             } else {
+//                 return false
+//             }
+//         } else {
+//             return false
+//         }
+//     }
+
+//     return !stack.length
+// }
+
+// Forth Solution
+const bracketMap = {
+    ")" : "(",
+    "}" : "{",
+    "]" : "["
+}
+
+var isValid = function(s) {
+    const str = []
+    
+    for (const char of s) {
+        if (bracketMap[char] && str.length < 1) {
             return false
+        } else if (bracketMap[char] && str[str.length - 1] === bracketMap[char]) {
+            str.pop()
+        } else {
+            str.push(char)
         }
     }
-
-    return !stack.length
-}
+    
+    return !str.length
+};
